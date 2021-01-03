d1bd542f71c18c98bdfd5d6708b7ea99b9295dea
Luka Dijan <ldijan00@fesb.hr>
2021-01-03 18:06:35 +0100

    login page

--- webui/app/src/App.js
+++ webui/app/src/App.js
@@ -1,211 +1,32 @@
-import React from 'react'
-import './App.css';
-import ChartContainer from './components/ChartContainer'
-import Kartica2 from './components/Card2'
-import DeviceTable from './components/DeviceTable';
-import MyNavbar from './components/NavBar'
-import Title from './components/Title'
-import { ThemeProvider, createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
-import { Card, CardContent, CssBaseline, Grid, Paper} from '@material-ui/core';
-import '@fontsource/poppins'
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './home'
+import Login from './components/login'
+
+
 
 class App extends React.Component {
   constructor() {
     super()
-    this.state = {dark: window.matchMedia('(prefers-color-scheme: dark)').matches}
-    this.themeDark = createMuiTheme({
-      palette: {
-        primary: {
-          main: '#2a8bf4',
-          chartTitle: '#43464a',
-          chartText: '#5a5b5c'
-        },
-        text: {
-          primary: "#d3d7db"
-        },
-        background: {
-          default: "#1f1e2e",
-          paper: "#28293d"
-        },
-        secondary: {
-          main: '#f57f17',
-        },
-      },
-      root: {
-        display: 'flex',
-      },
-      menuButton: {
-        marginRight: 36,
-      },
-      menuButtonHidden: {
-        display: 'none',
-      },
-      title: {
-        flexGrow: 1,
-      },
-      content: {
-        flexGrow: 1,
-        height: '100vh',
-        overflow: 'auto',
-      },
-      paper: {
-        display: 'flex',
-        overflow: 'auto',
-        flexDirection: 'column',
-      },
-      fixedHeight: {
-        height: 240,
-      },
-      typography: {
-        fontFamily: [
-          'Poppins',
-          'Roboto',
-          '"Helvetica Neue"',
-          'Arial',
-          'sans-serif'
-        ].join(','),
-      },
-      card: {
-        background: "rgb(70, 0, 0)",
-      },
-      page: {
-        backgroundColor: "rgb(70,80,90)"
-      },
-      table: {
-        border: "none",
-      },
-      overrides: {
-        MuiDataGrid: {
-          root: {
-            border: "none",
-            },
-            columnSeparator: {
-              color: "rgba(0,0,0,0)",
-            }
-      }
-    }
-    });
-    this.themeDark = responsiveFontSizes(this.themeDark)
-
-    this.themeLight = createMuiTheme({
-      palette: {
-        primary: {
-          main: '#2a8bf4',
-          chartTitle: '#fafafa',
-          chartText: '#f2f2f2'
-        },
-        text: {
-          primary: "#1f1e2e"
-        },
-        background: {
-          default: "#fafafa",
-          paper: "#f2f2f2"
-        },
-        secondary: {
-          main: '#f57f17',
-        },
-      },
-      root: {
-        display: 'flex',
-      },
-      menuButton: {
-        marginRight: 36,
-      },
-      menuButtonHidden: {
-        display: 'none',
-      },
-      title: {
-        flexGrow: 1,
-      },
-      content: {
-        flexGrow: 1,
-        height: '100vh',
-        overflow: 'auto',
-      },
-      paper: {
-        display: 'flex',
-        overflow: 'auto',
-        flexDirection: 'column',
-      },
-      fixedHeight: {
-        height: 240,
-      },
-      typography: {
-        fontFamily: [
-          'Poppins',
-          'Roboto',
-          '"Helvetica Neue"',
-          'Arial',
-          'sans-serif'
-        ].join(','),
-      },
-      card: {
-        background: "rgb(70, 0, 0)",
-      },
-      page: {
-        backgroundColor: "rgb(70,80,90)"
-      },
-      table: {
-        border: "none",
-      },
-      overrides: {
-        MuiDataGrid: {
-          root: {
-            border: "none",
-            },
-            columnSeparator: {
-              color: "rgba(0,0,0,0)",
-            }
-      }
-    }
-    });
-    this.themeLight = responsiveFontSizes(this.themeLight)
+    this.state = {}
   }
 
   render() {
-    return (
-      <ThemeProvider theme={this.state.dark ? this.themeDark : this.themeLight}>
-        <CssBaseline />
-        <div>
-          <MyNavbar />
-          <Grid container spacing={3} justify={"center"} direction={"column"} style={{width: "100%"}}>
-            <Grid item container justify={"center"} spacing={3} direction={"row"}>
-              <Grid item xs={10}>
-                <Paper>
-                  <Kartica2 />
-                </Paper>
-              </Grid>
-            </Grid>
 
-            <Grid item container xs={12} justify={"center"}>
-              <Grid item xs={10}>
-                <Card title={"WAN Bandwidth"}>
-                  <CardContent>
-                    <Title>
-                      WAN Bandwidth
-                    </Title>
-                    <ChartContainer/>
-                  </CardContent>
-                </Card>
-              </Grid>
-            </Grid>
-            <Grid item container xs={12} justify={"center"}>
-              <Grid item xs={10}>
-              <Card title={"Online uređaji"}>
-                  <CardContent>
-                    <Title>
-                      Online uređaji
-                    </Title>
-                    <DeviceTable/>
-                  </CardContent>
-                </Card>
-              </Grid>
-            </Grid>
-          </Grid>
-        </div>
-      </ThemeProvider>
+    return(
+      <Router>
+        <Switch>
+        <Route exact path="/home" component = {Home}/>
+        <Route exact path="/" component ={Login}/>
+        </Switch>
+      </Router>
     )
-  };
+  }
 }
 
 export default App;
--- webui/app/src/components/login.js
+++ webui/app/src/components/login.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react'
 import Card from '@material-ui/core/Card'
+import Grid from '@material-ui/core/Grid';
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Paper from '@material-ui/core/Paper'
+import { blue } from '@material-ui/core/colors';
+import Divider from '@material-ui/core/Divider'
+import TextField from '@material-ui/core/TextField'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+  import Home from '../home'
+  import Button from '@material-ui/core/Button'
 
-class Login extends Component {
+class Login extends React.Component {
     constructor() {
         super()
         this.state = {}
@@ -9,10 +25,39 @@ class Login extends Component {
 
     render() {
         return(
+            <Grid container container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: '100vh' }}>
+            <Grid item xs={3}>
 
-            <Card variant="outlined">
-                <h1>Hello</h1>
-            </Card>
+            {/* <Card variant="outlined">
+                <CardHeader>
+                <AccountCircleIcon fontSize="large"/>
+                </CardHeader>
+
+                <CardContent>
+                <h2>Sadrzaj</h2>
+                </CardContent>
+            </Card> */}
+
+            <Paper>
+                <AccountCircleIcon fontSize="large" style={{color: blue}}/>
+                <Divider/>
+                <form noValidate autoComplete="off">
+                <TextField label="name"/>
+                <TextField label="password"/>
+                </form>
+                <Divider/>
+                <Button>
+                <a href="../home">Login</a>
+                </Button>
+            </Paper>
+
+            </Grid>
+            </Grid>
         )
     }
 }
